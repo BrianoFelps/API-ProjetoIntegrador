fd7e8b34103c364971289a4966f7e70464ca18f0
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2024-05-20 14:16:17 -0300

    Enfoque EmojiMenu

--- models/EmojiMenu.js
+++ models/EmojiMenu.js
@@ -45,23 +45,6 @@ export const getEmojiByMenuId = async () => {
   });
 };
 
-// export const getEmojiByIdmenu = async (id) => {
-//   return new Promise((resolve, reject) => {
-//     const sql = 'SELECT em.id as id, e.emoji as emoji FROM emojimenu em JOIN emojis e ON em.id_emoji = e.id WHERE em.id = ?';
-//     db.query(sql, [id], (error, results) => {
-//       if (error) {
-//         reject(error);
-//         return;
-//       }
-//       if (results.length === 0) {
-//         resolve(null); // EmojiMenu não encontrado
-//         return;
-//       }
-//       resolve(results[0]);
-//     });
-//   });
-// }
-
 export const getEmojiByIdMenu = (id) => {
   return new Promise((resolve, reject) => {
     const sql = 'SELECT em.id as id, e.emoji as emoji FROM emojimenu em JOIN emojis e ON em.id_emoji = e.id WHERE em.id = ?';
