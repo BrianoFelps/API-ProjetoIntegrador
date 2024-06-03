fd31509981073b69b2a94ca4ec5f7b209d7df320
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2024-06-03 17:22:27 -0300

    Integração API no servidor, BackEnd do Equilibrium

--- database/db.js
+++ database/db.js
@@ -1,20 +1,30 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+// Dinamismo da conexão ao bd
 export const db = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "admin",
-    database: "ProjetoIntegradorWEB"
+    // host: "localhost",
+    // port: 3306,
+    // user: "root",
+    // password: "admin",
+    // database: "ProjetoIntegradorWEB"
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 })
 
-db.connect((err) => {
-    if(err){
-        console.log(`Erro de conexão ao banco de dados: ${err}`);
-        return;
+db.then(() => {
+    console.log(`Conexão bem-sucedida ao banco de dados MySQL (${process.env.MYSQLDATABASE})`)
+    if(process.env.MYSQLHOST === 'localhost'){
+        console.log(`Usando servidor local`);
+        
     }
-
-    console.log(`Conexão bem-sucedida!`)
-})
+}).catch(err =>{
+    console.log(`Erro ao se conectar ao banco de dados (${process.env.MYSQLDATABASE}): ${err}`)
+});
 
 export default db;
