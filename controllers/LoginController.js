3890f5f1c165d33603a0b29f317fc79512ace6d5
arthurpelitoo <arthur.pelito@escola.pr.gov.br>
2024-06-10 15:19:00 -0300

    RespostaDBIdNomeEmailporMeioDoUsuario

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -14,7 +14,7 @@ export const verificarlogin = (req, res) => {
         } else {
             if(data.length > 0) {
                 console.log("Login bem-sucedido");
-                return res.status(200).json({ nome: data[0].nome });
+                return res.status(200).json({ id: data[0].id, nome: data[0].nome, email: data[0].email });
             } else {
                 console.log("Credenciais inválidas");
                 return res.status(401).json({ error: "Credenciais inválidas" });
