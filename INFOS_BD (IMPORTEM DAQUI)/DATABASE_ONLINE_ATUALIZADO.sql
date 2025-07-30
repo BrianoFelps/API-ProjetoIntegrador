5e8918ce62ae07e27f373a69a79d71ac4349fc3c
BrianoFelps <briansticknodesbr@gmail.com>
2025-07-30 15:24:47 -0300

    dump corrigido, BD e conexão mysql-DBEAVER criado

--- INFOS_BD (IMPORTEM DAQUI)/DATABASE_ONLINE_ATUALIZADO.sql
+++ INFOS_BD (IMPORTEM DAQUI)/DATABASE_ONLINE_ATUALIZADO.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `elements`
 --
 
+USE railway;
+
 DROP TABLE IF EXISTS `elements`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
