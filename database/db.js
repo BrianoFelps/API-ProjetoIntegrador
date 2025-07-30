b076f10bded1b948f32f7ee26cc8fc57eefc173d
BrianoFelps <briansticknodesbr@gmail.com>
2025-07-30 15:54:58 -0300

    Correção 2

--- database/db.js
+++ database/db.js
@@ -10,11 +10,12 @@ export const db = mysql.createConnection({
     // user: "root",
     // password: "admin",
     // database: "ProjetoIntegradorWEB"
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+
 })
 
 db.connect((err) => {
